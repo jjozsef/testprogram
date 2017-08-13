c8357073767b716a02176db4805383a51845a60f
jjozsef <juhaszjozsef1987@gmail.com>
2017-08-13 20:45:48 +0200

    Initial commit

---
+++ src/com/juhaszjozsef/Main.java
@@ -0,0 +1,15 @@
+package com.juhaszjozsef;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("elso");
+
+        link.egy();
+
+        int szam = link.ketto(111);
+
+        System.out.println(szam + 4);
+    }
+}
---
+++ src/com/juhaszjozsef/link.java
@@ -0,0 +1,15 @@
+package com.juhaszjozsef;
+
+public class link {
+
+    static void egy() {
+        System.out.println("masodik");
+    }
+
+    int b = 10;
+
+    static int ketto(int b) {
+        int a = 1;
+        return a + b + 5;
+    }
+}
